e10415af25ae2c4c200ecc3864eef27444e9daae
Shamil <badalloffsh@gmail.com>
2021-02-28 15:17:47 +0100

    Initial run

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+            <Route path="/signin">
+                <p>I will be the sign in page</p>
+            </Route>
+            <Route path="/signup">
+                <p>I will be the sign up page</p>
+            </Route>
+            <Route path="/browse">
+                <p>I will be the browse page</p>
+            </Route>
+            <Route path="/">
+                <p>I am going to be a cloned Netflix application</p>
+            </Route>
+        </Switch>
+    </Router>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I am going to be a cloned Netflix application/i);
   expect(linkElement).toBeInTheDocument();
 });
