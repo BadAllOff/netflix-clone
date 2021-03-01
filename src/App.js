a28987c5b56b5dbc6d701ff560849dc15101deb5
Shamil <badalloffsh@gmail.com>
2021-03-01 17:17:17 +0100

    Add: basic jumbotron styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Home from "./pages/home";
 
-function App() { 
+import "./App.css";
+
+function App() {
   return (
     <Router>
-        <Switch>
-            <Route path="/signin">
-                <p>I will be the sign in page</p>
-            </Route>
-            <Route path="/signup">
-                <p>I will be the sign up page</p>
-            </Route>
-            <Route path="/browse">
-                <p>I will be the browse page</p>
-            </Route>
-            <Route path="/">
-                <p>I am going to be a cloned Netflix application</p>
-            </Route>
-        </Switch>
+      <Switch>
+        <Route path="/signin">
+          <p>I will be the sign in page</p>
+        </Route>
+        <Route path="/signup">
+          <p>I will be the sign up page</p>
+        </Route>
+        <Route path="/browse">
+          <p>I will be the browse page</p>
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   );
 }
