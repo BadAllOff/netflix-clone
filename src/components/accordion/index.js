0b50ee2491918a5aa9583449762e1c63c825a3dc
Shamil <badalloffsh@gmail.com>
2021-03-04 12:42:45 +0100

    Add: accordion compound component structure

---
+++ src/components/accordion/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Body, Title, Header, Container, Inner, Item } from './styles/accordion';
+
+export default function Accordion({ children, ...restProps }) {
+    return (
+        <Container {...restProps}>
+            <Inner>{children}</Inner>
+        </Container>
+    )
+}
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+    return <Item {...restProps}>{children}</Item>
+}
+
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+}
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+    return <Header {...restProps}>{children}</Header>;
+}
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+    return <Body {...restProps}>{children}</Body>;
+}
