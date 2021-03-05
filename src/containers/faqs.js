1d3ba35ea3fa4fef851824a2e04a1ea5ee26dc40
Shamil <badalloffsh@gmail.com>
2021-03-05 19:46:41 +0100

    Add: OptForm compound component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { Accordion } from '../components';
-import faqsData from '../fixtures/faqs.json'
+import React from "react";
+import { Accordion, OptForm } from "../components";
+import faqsData from "../fixtures/faqs.json";
 
 export function FaqsContainer() {
-    return (
-        <Accordion>
-            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            <Accordion.Frame>
-                {faqsData.map((item) => (
-                    <Accordion.Item key={item.id}>
-                        <Accordion.Header>{item.header}</Accordion.Header>
-                        <Accordion.Body>{item.body}</Accordion.Body>
-                    </Accordion.Item>
-                ))}
-            </Accordion.Frame>
-        </Accordion>);
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      <Accordion.Frame>
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion.Frame>
+
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+      </OptForm>
+    </Accordion>
+  );
 }
