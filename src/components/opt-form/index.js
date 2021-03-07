47cad0c230d669f87cdd7afd0cd1a695ff8173d6
Shamil <badalloffsh@gmail.com>
2021-03-07 23:27:24 +0100

    Modfy: optform

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Input, Button, Text } from './styles/opt-form';
+import { Container, Input, Break, Button, Text } from './styles/opt-form';
 
 export default function OptForm({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
@@ -15,6 +15,10 @@ OptForm.Button = function OptFormButton({ children, ...restProps }) {
             {children} <img src="/images/icons/chevron-right.png" alt="Try Now" />
         </Button>
     )
+}
+
+OptForm.Break = function OptBreak({ ...restProps }) {
+    return <Break {...restProps} />;
 }
 
 OptForm.Text = function OptFormText({ children, ...restProps }) {
