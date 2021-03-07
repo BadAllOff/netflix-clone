eff231b7bcd87ad7d6161526fb455920378d8c9f
Shamil <badalloffsh@gmail.com>
2021-03-07 23:33:45 +0100

    Add: Adding a Routes Constant File

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import { Route } from "react-router-dom";
-import Home from "./pages/home";
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import Home from './pages/home';
 
-function App() {
+export function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/signin">
-          <p>I will be the sign in page</p>
-        </Route>
-        <Route path="/signup">
-          <p>I will be the sign up page</p>
-        </Route>
-        <Route path="/browse">
-          <p>I will be the browse page</p>
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+        <Switch>
+            <Route path={ROUTES.SIGN_IN}>
+                <p>I will be the sign in page</p>
+            </Route>
+            <Route path={ROUTES.SIGN_UP}>
+                <p>I will be the sign up page</p>
+            </Route>
+            <Route path={ROUTES.BROWSE}>
+                <p>I will be the browse page</p>
+            </Route>
+            <Route path={ROUTES.HOME}>
+                <Home />
+            </Route>
+        </Switch>
     </Router>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
-import App from './App';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
