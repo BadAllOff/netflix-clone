48d26080d2c4198e119af1c84e9b17e444b87265
Shamil <badalloffsh@gmail.com>
2021-03-08 22:44:27 +0100

    Add: building out the header compound component

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { FaqsContainer } from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
 
 export default function Home() {
     return (
         <>
+            <HeaderContainer>
+                <p>Hello</p>
+            </HeaderContainer>
+            
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
