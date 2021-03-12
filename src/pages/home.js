b4d6e39b7c13c44803b6accef15367fcf55c01ea
Shamil <badalloffsh@gmail.com>
2021-03-12 11:57:14 +0100

    Add: styling the feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import { Feature, OptForm } from '../components';
-import { FaqsContainer } from '../containers/faqs';
-import { JumbotronContainer } from '../containers/jumbotron';
-import { FooterContainer } from '../containers/footer';
-import { HeaderContainer } from '../containers/header';
+import React from "react";
+import { Feature, OptForm } from "../components";
+import { FaqsContainer } from "../containers/faqs";
+import { JumbotronContainer } from "../containers/jumbotron";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
 
 export default function Home() {
-    return (
-        <>
-            <HeaderContainer>
-                <OptForm>
-                    <Feature>
-                        <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
-                        <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
-                        <OptForm.Input placeholder="Email Address" />
-                        <OptForm.Button>Try it now</OptForm.Button>
-                        <OptForm.Break />
-                        <OptForm.Text>Ready to watch? Enter your email to create or restart
-                        your membership.</OptForm.Text>
-                    </Feature>
-                </OptForm>
-            </HeaderContainer>
-            
-            <JumbotronContainer />
-            <FaqsContainer />
-            <FooterContainer />
-        </>
-    )
+  return (
+    <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
 }
