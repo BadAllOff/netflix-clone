0a9a81213a54f1e52ae154c4f28d2dba0596151b
Shamil <badalloffsh@gmail.com>
2021-03-14 20:25:17 +0100

    Add: form component sceleton

---
+++ src/components/form/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Form({children, ...restProps}) {
+    return <Container {...restProps}>{children}</Container>
+}
+
+Form.Base = function FormBase({children, ...restProps}) {
+    return <Base {...restProps}>{children}</Base>
+}
+
+Form.Title = function FormTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+}
+
+Form.Text = function FormText({children, ...restProps}) {
+    return <Text {...restProps}>{children}</Text>
+}
+
+Form.TextSmall = function FormTextSmall({children, ...restProps}) {
+    return <TextSmall {...restProps}>{children}</TextSmall>
+}
+
+Form.Error = function FormError({children, ...restProps}) {
+    return <Error {...restProps}>{children}</Error>
+}
+
+Form.input = function FormInput({children, ...restProps}) {
+    return <Input {...restProps}>{children}</Input>
+}
+
+Form.Submit = function FormSubmit({children, ...restProps}) {
+    return <Submit {...restProps}>{children}</Submit>
+}
