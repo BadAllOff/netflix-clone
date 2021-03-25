ee61d787c93f2e6905494ca36bc6024e0445aeca
Shamil <badalloffsh@gmail.com>
2021-03-25 16:20:39 +0100

    Add: browse page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import { Home, Signin, Signup } from './pages';
+import { Home, Signin, Signup, Browse } from './pages';
 
 export function App() {
   return (
@@ -15,7 +15,7 @@ export function App() {
           <Signup />
         </Route>
         <Route path={ROUTES.BROWSE}>
-          <p>I will be the browse page</p>
+          <Browse />
         </Route>
         <Route path={ROUTES.HOME}>
           <Home />
