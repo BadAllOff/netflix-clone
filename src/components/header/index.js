6416e3a8783f679f568370c5fa8486abe52fb41a
Shamil <badalloffsh@gmail.com>
2021-03-27 11:23:11 +0100

    Add: search to header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,61 +1,79 @@
-import React from "react";
-import { Link as ReachRouterLink } from "react-router-dom";
-import {
-  Group,
-  Background,
-  Link,
-  Text,
-  Container,
-  Logo,
-  ButtonLink,
-  Feature,
-  FeatureCallOut,
-  PlayButton,
-} from "./styles/header";
+import React, { useState } from 'react';
+import { Link as ReachRouterLink } from 'react-router-dom';
+import { 
+    Group, 
+    Background, 
+    Link, 
+    Text, 
+    Container, 
+    Logo, 
+    ButtonLink,
+    Feature,
+    FeatureCallOut,
+    PlayButton,
+    Search,
+    SearchIcon,
+    SearchInput
+} from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
-  return bg ? <Background {...restProps}>{children}</Background> : children;
+    return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
-  return <Container {...restProps}>{children}</Container>;
-};
+    return <Container {...restProps}>{children}</Container>;
+}
 
 Header.Group = function HeaderGroup({ children, ...restProps }) {
-  return <Group {...restProps}>{children}</Group>;
-};
+    return <Group {...restProps}>{children}</Group>;
+}
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
+    const [searchActive, setSearchActive] = useState(false);
+    
+    return (
+        <Search {...restProps}>
+            <SearchIcon onClick={() => setSearchActive(!searchActive)}>
+                <img src="/images/icons/search.png" alt="Search" />
+            </SearchIcon>
+            <SearchInput
+                value={searchTerm}
+                onChange={({ target }) => setSearchTerm(target.value)}
+                placeholder="Search files and series"
+                active={searchActive}
+            />
+        </Search>
+    )
+}
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
-  return (
-    <ReachRouterLink to={to}>
-      <Logo {...restProps} />
-    </ReachRouterLink>
-  );
-};
+    return (
+        <ReachRouterLink to={to}>
+            <Logo {...restProps} />
+        </ReachRouterLink>
+    )
+}
 
 Header.Text = function HeaderText({ children, ...restProps }) {
-  return <Text {...restProps}>{children}</Text>;
-};
+    return <Text {...restProps}>{children}</Text>;
+}
 
 Header.Link = function HeaderLink({ children, ...restProps }) {
-  return <Link {...restProps}>{children}</Link>;
-};
+    return <Link {...restProps}>{children}</Link>;
+}
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
-  return <ButtonLink {...restProps}>{children}</ButtonLink>;
-};
+    return <ButtonLink {...restProps}>{children}</ButtonLink>;
+}
 
 Header.Feature = function HeaderFeature({ children, ...restProps }) {
-  return <Feature {...restProps}>{children}</Feature>;
-};
+    return <Feature {...restProps}>{children}</Feature>
+}
 
-Header.FeatureCallOut = function HeaderFeatureCallOut({
-  children,
-  ...restProps
-}) {
-  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
-};
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
 
 Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
-  return <PlayButton {...restProps}>{children}</PlayButton>;
-};
+    return <PlayButton {...restProps}>{children}</PlayButton>
+}
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,111 +1,108 @@
-import styled from "styled-components";
-import { Link as ReachRouterLink } from "react-router-dom";
+import styled from 'styled-components';
+import { Link as ReachRouterLink } from 'react-router-dom';
 
 export const Background = styled.section`
-  display: flex;
-  flex-direction: column;
-  background: url(${({ src }) =>
-      src ? `../images/misc/${src}.jpg` : "../images/misc/home-bg.jpg"})
-    top left / cover no-repeat;
-
-  @media (max-width: 1100px) {
-    ${({ dontShowOnSmallViewPort }) =>
-      dontShowOnSmallViewPort && `background: none;`}
-  }
-`;
+    display: flex;
+    flex-direction: column;
+    background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover no-repeat;
+    
+    @media (max-width: 1100px) {
+        ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
+    }
+`
 
 export const Container = styled.div`
-  display: flex;
-  margin: 0 56px;
-  height: 64px;
-  padding: 18px 0;
-  justify-content: space-between;
-  align-items: center;
-
-  a {
     display: flex;
-  }
-
-  @media (max-width: 1000px) {
-    margin: 0 30px;
-  }
+    margin: 0 56px;
+    height: 64px;
+    padding: 18px 0;
+    justify-content: space-between;
+    align-items: center;
+    
+    a {
+        display: flex;
+    }
+    
+    @media (max-width: 1000px) {
+        margin: 0 30px;
+    }
 `;
 
 export const Link = styled.p`
-  color: #fff;
-  text-decoration: none;
-  margin-right: 30px;
-  font-weight: ${({ active }) => (active === "true" ? "700" : "normal")};
-  cursor: pointer;
-
-  &:hover {
-    font-weight: bold;
-  }
-  &:last-of-type {
-    margin-right: 0;
-  }
+    color: #fff;
+    text-decoration: none;
+    margin-right: 30px;
+    font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
+    cursor: pointer;
+    
+    &:hover {
+        font-weight: bold;
+    }
+    &:last-of-type {
+        margin-right: 0;
+    }
 `;
 
 export const ButtonLink = styled(ReachRouterLink)`
-  display: block;
-  background-color: #e50914;
-  width: 84px;
-  height: fit-content;
-  color: white;
-  border: 0;
-  font-size: 15px;
-  border-radius: 3px;
-  padding: 8px 17px;
-  cursor: pointer;
-  text-decoration: none;
-  box-sizing: border-box;
-
-  &:hover {
-    background: #f40612;
-  }
+    display: block;
+    background-color: #e50914;
+    width: 84px;
+    height: fit-content;
+    color: white;
+    border: 0;
+    font-size: 15px;
+    border-radius: 3px;
+    padding: 8px 17px;
+    cursor: pointer;
+    text-decoration: none;
+    box-sizing: border-box;
+    
+    &:hover {
+        background: #f40612;
+    }
 `;
 
 export const Logo = styled.img`
-  height: 32px;
-  width: 108px;
-  margin-right: 40px;
-
-  @media (min-width: 1449px) {
-    height: 45px;
-    width: 167px;
-  }
+    height: 32px;
+    width: 108px;
+    margin-right: 40px;
+    
+    @media (min-width: 1449px) {
+        height: 45px;
+        width: 167px;
+    }
 `;
 
 export const Group = styled.div`
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 `;
 
 export const Text = styled.p`
-  color: white;
-  font-size: 22px;
-  line-height: normal;
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+    color: white;
+    font-size: 22px;
+    line-height: normal;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
 `;
 
 export const Feature = styled(Container)`
-  padding: 150px 0 500px 0;
-  flex-direction: column;
-  align-items: normal;
-  width: 50%;
-
-  @media (max-width: 1100px) {
-    display: none;
-  }
+    padding: 150px 0 500px 0;
+    flex-direction: column;
+    align-items: normal;
+    width: 50%;
+    
+    @media (max-width: 1100px) {
+        display: none;
+    }
 `;
 
 export const FeatureCallOut = styled.h2`
-  color: white;
-  font-size: 50px;
-  line-height: normal;
-  font-weight: bold;
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
-  margin: 0;
+    color: white;
+    font-size: 50px;
+    line-height: normal;
+    font-weight: bold;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+    margin: 0;
 `;
 
 export const PlayButton = styled.button`
@@ -126,4 +123,37 @@ export const PlayButton = styled.button`
         background: #ff1e1e;
         color: white;
     }
+`;
+
+export const Search = styled.div`
+    display: flex;
+    align-items: center;
+    
+    @media (max-width: 700px) {
+        display: none;
+    }
+`;
+
+export const SearchIcon = styled.button`
+    cursor: pointer;
+    background-color: transparent;
+    border: 0;
+    
+    img {
+        filter: brightness(0) invert(1);
+        width: 16px;
+    }
+`
+
+export const SearchInput = styled.input`
+    background-color: #44444459;
+    color: white;
+    border: 1px solid white;
+    transition: width 0.5s;
+    height: 30px;
+    font-size: 14px;
+    margin-left: ${({ active }) => (active === true ? '10px' : '0')};
+    padding: ${({ active }) => (active === true ? '0 10px' : '0')};
+    opacity: ${({ active }) => (active === true ? '1' : '0')};
+    width: ${({ active }) => (active === true ? '200px' : '0px')};
 `;
