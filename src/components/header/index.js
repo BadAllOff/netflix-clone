ffa493cc091ea20051b0926c72104d6d8342b86a
Shamil <badalloffsh@gmail.com>
2021-03-27 10:46:24 +0100

    Add: header to browse container

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link as ReachRouterLink } from 'react-router-dom';
-import { Background, Container, Logo, ButtonLink } from './styles/header';
+import { Group, Background, Link, Text, Container, Logo, ButtonLink } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -10,12 +10,24 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
 }
 
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>;
+}
+
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
     return (
         <ReachRouterLink to={to}>
             <Logo {...restProps} />
         </ReachRouterLink>
     )
+}
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+}
+
+Header.Link = function HeaderLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>;
 }
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,21 +1,50 @@
-import React, { useState } from 'react';
-import { Header } from '../components';
-import * as ROUTES from '../constants/routes';
-import { FirebaseContext } from '../context/firebase';
-import { SelectProfileContainer } from './profiles';
-import { FooterContainer } from './footer';
+import React, { useState, useContext } from "react";
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import { FirebaseContext } from "../context/firebase";
+import { SelectProfileContainer } from "./profiles";
+import { FooterContainer } from "./footer";
 
 export function BrowseContainer() {
-    const [profile, setProfile] = useState({});
-    
-    const user = {
-        displayName: "Karl",
-        photoURL: "1"
-    };
-    
-    return profile.displayName ? (
-        <><p>Browse Container</p>
-            <FooterContainer />
-        </>)
-        : (<SelectProfileContainer user={user} />);
+  const [category, setCategory] = useState("series");
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  const user = {
+    displayName: "Karl",
+    photoURL: "1",
+  };
+
+  return profile.displayName ? (
+    <>
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo
+              to={ROUTES.HOME}
+              src="/images/misc/logo.svg"
+              alt="Netflix"
+            />
+            <Header.Link
+              active={category === "series" ? "true" : "false"}
+              onClick={() => setCategory("series")}
+            >
+              Series
+            </Header.Link>
+            <Header.Link
+              active={category === "films" ? "true" : "false"}
+              onClick={() => setCategory("films")}
+            >
+              Films
+            </Header.Link>
+          </Header.Group>
+        </Header.Frame>
+      </Header>
+      <FooterContainer />
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
