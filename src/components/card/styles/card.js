909e4813f26c2d1667e240901f82a08d1ab61f68
Shamil <badalloffsh@gmail.com>
2021-04-05 22:36:47 +0200

    Add: basic styling for feature card ;

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -26,19 +26,97 @@ export const Container = styled.div`
     }
 `;
 
-export const Group = styled.div``;
+export const Group = styled.div`
+    display: flex;
+    flex-direction: ${({ flexDirection }) => (flexDirection === 'row' ? 'row' : 'column')};
 
-export const SubTitle = styled.p``;
+    
+    > ${Container}:first-of-type {
+        @media (min-width: 1100px) {
+            margin-top: -150px;
+        }
+    }
+`;
 
-export const Text = styled.p``;
+export const SubTitle = styled.p`
+    font-size: 12px;
+    color: white;
+    font-weight: bold;
+    margin-top: 0;
+    margin-bottom: 0;
+    user-select: none;
+    display: none;
+`;
 
-export const Entities = styled.div``;
+export const Text = styled.p`
+    margin-top: 5px;
+    font-size: 10px;
+    color: white;
+    margin-bottom: 0;
+    user-select: none;
+    display: none;
+    line-height: normal;
+`;
 
-export const Meta = styled.div``;
+export const Entities = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
 
-export const Image = styled.img``;
+export const Meta = styled.div`
+    display: none;
+    position: absolute;
+    bottom: 0;
+    padding: 10px;
+    background-color: #0000008f;
+`;
 
-export const Item = styled.div``;
+export const Image = styled.img`
+    border: 0;
+    width: 100%;
+    max-width: 305px;
+    cursor: pointer;
+    height: auto;
+    padding: 0;
+    margin: 0;
+`;
+
+export const Item = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-right: 5px;
+    position: relative;
+    cursor: pointer;
+    transition: transform 0.2;
+    
+    &:hover {
+        transform: scale(1.3);
+        z-index: 99;
+    }
+    
+    @media (min-width: 1200px) {
+        &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle} {
+            display: block;
+            z-index: 100;
+        }
+    }
+    
+    &:first-of-type {
+        margin-left: 56px;
+        
+        @media (max-width: 1000px) {
+            margin-left: 30px;
+        }
+    }
+    
+    &:last-of-type {
+        margin-right: 56px;
+        
+        @media (max-width: 1000px) {
+            margin-right: 30px;
+        }
+    }
+`;
 
 export const FeatureText = styled.p``;
 
