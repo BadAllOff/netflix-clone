d7844856ea67578ab10ee52e7a1bde9a8f5e9c59
Shamil <badalloffsh@gmail.com>
2021-04-12 21:54:55 +0200

    Add: implementing custom auth hook to app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import { Home, Signin, Signup, Browse } from './pages';
+import { Home, Browse, Signin, Signup } from "./pages";
+import { useAuthListener } from "./hooks";
 
 export function App() {
+  const { user } = useAuthListener();
+
   return (
     <Router>
       <Switch>
